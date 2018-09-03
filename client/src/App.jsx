fc24863dc34cc7da49ea72dc448a4770add2265f
Robert Gao <gaorober@usc.edu>
2018-09-02 18:55:14 -0700

    (init)

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Hurray, everything installed correctly!  Now get to work.</div>
+    )
+  }
+}
+
+export default App;
